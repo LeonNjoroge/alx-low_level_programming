25f39f2bbb50e7f55e3aba68e048173ba65a7cdb
Leon <leonmuraguri@gmail.com>
2023-09-07 05:00:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.c>
+int main(void)
+{
+  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+    for (int 1 =0; message[i]!= '\0'; i++)
+    {
+     fputc(message[i],stderr);
+    }
+
+  return (1);
+}
