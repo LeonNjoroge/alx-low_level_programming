fd25ed509845f40c03aa14831bc338e858b0a210
Leon <leonmuraguri@gmail.com>
2023-09-07 05:02:41 -0700

    101-quote.c, changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-    for (int 1 =0; message[i]!= '\0'; i++)
+  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    for (int i =0; message[i]!= '\0'; i++)
     {
      fputc(message[i],stderr);
     }
