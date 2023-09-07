1ddb9357a31f1a67013dd54e03c74564acf80433
Leon <leonmuraguri@gmail.com>
2023-09-07 05:46:32 -0700

    Betty checks 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+/**
+ *main - entry point
+ *Loops through the array of characters and prints them
+ *Return:Always 1(Error)
+ */
 int main(void)
 {
-  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  int i=0;
-  for (; message[i]!= '\0'; i++)
-  {
-    fputc(message[i],stderr);
-  }
-
-  return (1);
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int i=0;
+for (; message[i]!= '\0'; i++)
+{
+  fputc(message[i],stderr);
+}
+return (1);
 }
