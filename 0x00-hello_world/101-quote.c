4358592d2b3efec623a0bf64b76d1262634414f3
Leon <leonmuraguri@gmail.com>
2023-09-07 05:48:48 -0700

    Betty checks 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-int i=0;
-for (; message[i]!= '\0'; i++)
+int i = 0;
+for (message[i] != '\0'; i++)
 {
-  fputc(message[i],stderr);
+fputc(message[i], stderr);
 }
 return (1);
 }
