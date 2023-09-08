f34d3c28f2eb53bb790c288c38d51fb77db7ceaf
Leon <leonmuraguri@gmail.com>
2023-09-08 13:53:23 -0700

    Solution 5 - 6

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints lowercase letters from 'a' to 'z', excluding 'e' and 'q'.
+ * Description: Prints lowercase letters\
+ from 'a' to 'z', excluding 'e' and 'q'.
  *
  * Return: Always 0 (Success)
  */
