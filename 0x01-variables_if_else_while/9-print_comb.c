a6b7d822a40fd5c99805bc4b05824cfeea739652
Leon <leonmuraguri@gmail.com>
2023-09-08 12:45:16 -0700

    Solution 10 - 1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,9 +5,16 @@ int main()
   int i = 0;
   for (; i <= 9; i++)
   {
+    if (i != 9)
+    {
     putchar('0' + i);
     putchar(',');
     putchar(' ');
+    }
+    else
+    {
+      break;
+    }  
   }
   putchar('\n');
   return (0);
