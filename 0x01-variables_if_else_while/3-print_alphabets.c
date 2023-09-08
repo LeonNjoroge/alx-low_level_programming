ece428f3cdb23ddab9b2458b1d49b031ff11d3e6
Leon <leonmuraguri@gmail.com>
2023-09-08 13:50:05 -0700

    Solution 4 - 5

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints lowercase letters from 'a' to 'z' followed by
+ * uppercase letters from 'A' to 'Z'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char lower_case = 'a';
-  char upper_case = 'A';
+	char lower_case = 'a';
+	char upper_case = 'A';
 
-  while (lower_case <= 'z')
-  {
-    putchar(lower_case);
-    lower_case++;
-  }
-  while (upper_case <= 'Z')
-  {
-    putchar(upper_case);
-    upper_case++;
-  }
-  putchar('\n');
-  return (0);
+	while (lower_case <= 'z')
+	{
+		putchar(lower_case);
+		lower_case++;
+	}
+	while (upper_case <= 'Z')
+	{
+		putchar(upper_case);
+		upper_case++;
+	}
+	putchar('\n');
+	return (0);
 }
