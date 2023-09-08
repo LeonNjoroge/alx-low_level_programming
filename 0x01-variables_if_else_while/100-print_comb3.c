9fbfc92e3914dd075d4631e417fd82c1995bc7e0
Leon <leonmuraguri@gmail.com>
2023-09-08 14:05:01 -0700

    Solution 10 - 6

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 
-
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of two digits,
+ * separated by commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  int i;
-  int j;
-  for  (i = 0; i <= 8; i++)
-  {
-    for (j = i + 1; j <= 9; j++)
-    {
-      putchar(i + '0');
-      putchar(j + '0');
+	int i;
+	int j;
 
-      if (i != 8 || j != 9)
-      {
-	putchar(',');
-	putchar(' ');
-      }
-    }
-    
-  }
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
 
-  putchar('\n');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-  return (0);
+	putchar('\n');
+
+	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints numbers from 0 to 9 separated by commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  int i = 0;
-  for (; i <= 9; i++)
-  {
-    putchar('0' + i);
-    if(i != 9)
-    {
-    putchar(',');
-    putchar(' ');
-    }
-    else
-    {
-      break;
-    }  
-  }
-  putchar('\n');
-  return (0);
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		putchar('0' + i);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
