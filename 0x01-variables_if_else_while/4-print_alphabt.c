e78d34061d005392452db736e5aef89d887a785e
Leon <leonmuraguri@gmail.com>
2023-09-08 13:52:22 -0700

    Solution 5 - 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints lowercase letters from 'a' to 'z', excluding 'e' and 'q'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char letter = 'a';
+	char letter = 'a';
 
-  while (letter <= 'z')
-  {
-    if (letter != 'e' && letter != 'q')
-    {
-      putchar(letter);
-      letter++;
-    }
-    else
-    {
-      letter++;
-    }
-  }
-  putchar('\n');
-  return (0);
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
 }
