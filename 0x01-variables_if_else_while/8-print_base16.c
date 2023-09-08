bd27d60d5b1548c079993c4b68f7f15120622ad4
Leon <leonmuraguri@gmail.com>
2023-09-08 08:40:06 -0700

    Solution 8 - 3

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+  for (; i <= 9; i++)
+  {
+    putchar('0' + i);
+  }
+  char letter = 'a';
+
+  while (letter <= 'f')
+  {
+    putchar(letter);
+    letter++;
+  }
+  putchar('\n');
+  return (0);
+}
