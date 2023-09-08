8a81f2b327f48a94126c0b0bae358d5d2c4cd2b9
Leon <leonmuraguri@gmail.com>
2023-09-08 13:58:13 -0700

    Solution 7 - 5

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  int i = 0;
-  for (; i <= 9; i++)
-  {
-    putchar('0' + i);
-  }
-  putchar('\n');
-  return (0);
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+	return (0);
 }
