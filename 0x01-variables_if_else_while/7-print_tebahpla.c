6a821a762af9c30cb84d92702d18f2697b4720cc
Leon <leonmuraguri@gmail.com>
2023-09-08 14:00:55 -0700

    Solution 8 - 6

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints lowercase letters from 'z' to 'a' in reverse order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char letter = 'z';
+	char letter = 'z';
 
-  while (letter >= 'a')
-  {
-    putchar(letter);
-    letter--;
-  }
-  putchar('\n');
-  return (0);
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+	return (0);
 }
-    
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints numbers from 0 to 9 followed by lowercase letters
+ * from 'a' to 'f'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  int i = 0;
-  char letter;
-  for (; i <= 9; i++)
-  {
-    putchar('0' + i);
-  }
- 
-  letter = 'a';
+	int i = 0;
+	char letter;
 
-  while (letter <= 'f')
-  {
-    putchar(letter);
-    letter++;
-  }
-  putchar('\n');
-  return (0);
+	for (; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+
+	letter = 'a';
+
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
 }
