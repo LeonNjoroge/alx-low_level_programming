5d67af19b6a7601bebfe0a656c6f68fff073930e
Leon <leonmuraguri@gmail.com>
2023-09-12 11:33:36 -0700

    Betty Checker

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
-
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: This is the main function of the program. It prints the
+ * string "_putchar" character by character using the `_putchar` function
+ * and returns 0 to indicate successful execution.
+ */
 int main(void)
 {
-  char str[] = "_putchar\n";
-  int i = 0;
-  for (; str[i] != '\0'; i++)
-  {
-    _putchar(str[i]);
-  }
-  return(0);
+    char str[] = "_putchar\n";
+    int i = 0;
 
-}  
+    for (; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    return (0);
+}
