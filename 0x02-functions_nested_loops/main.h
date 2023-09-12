eae03e67d7cb8addfae9c7dabd20505d816abc8d
Leon <leonmuraguri@gmail.com>
2023-09-12 07:44:43 -0700

    Solution 0 - 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,18 @@
-void _putchar(char c);
+#include <unistd.h>
+
+void _putchar(char c)
+{
+  write(1, &c, 1);
+}  
+
+
+void print_alphabet_x10(void)
+{
+  char letter = 'a';
+  while(letter <= 'z')
+  {
+    _putchar(letter);
+    letter++;
+  }
+  _putchar("\n");
+}
