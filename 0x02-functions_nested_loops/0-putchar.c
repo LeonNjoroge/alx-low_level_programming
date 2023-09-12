7b013a1c1f002b0e12cfb116631011052d2dcfa9
Leon <leonmuraguri@gmail.com>
2023-09-12 11:39:28 -0700

    Solution 0 - 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,21 @@
 /**
  * main - Entry point of the program.
  *
- * Return: Always 0 (Success)
- *
  * Description: This is the main function of the program. It prints the
  * string "_putchar" character by character using the `_putchar` function
  * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  char str[] = "_putchar\n";
-  int i = 0;
-  for (; str[i] != '\0'; i++)
+    char str[] = "_putchar\n";
+    int i = 0;
+
+    for (; str[i] != '\0'; i++)
     {
-      _putchar(str[i]);
+        _putchar(str[i]);
     }
 
-  return (0);
+    return (0);
 }
