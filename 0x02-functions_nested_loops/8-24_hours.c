cc664247f45c449dfbb9188748f42f2ae39ef3de
Leon <leonmuraguri@gmail.com>
2023-09-12 12:07:53 -0700

    Solution 9 - 0

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,23 +3,24 @@
 /**
  * jack_bauer - Prints every minute of the day in the format HH:MM.
  *
- * Description: This function prints every minute of the day, starting from
+ * Description: This function prints
+ * every minute of the day, starting from
  * 00:00 and ending at 23:59, in the format HH:MM.
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar('0' + (hour / 10));
-            _putchar('0' + (hour % 10));
-            _putchar(':');
-            _putchar('0' + (minute / 10));
-            _putchar('0' + (minute % 10));
-            _putchar('\n');
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+		}
+	}
 }
