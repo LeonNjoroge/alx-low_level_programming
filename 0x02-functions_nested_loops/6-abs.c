d422ece9a225ac292723b9e03752b2ac268a1d89
Leon <leonmuraguri@gmail.com>
2023-09-12 12:01:33 -0700

    Solution 7 - 0

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,18 +7,20 @@
  *
  * Return: The absolute value of @num.
  *
- * Description: This function computes the absolute value of the input integer @num.
- * If @num is negative, it returns the negation of @num to make it positive. If @num
+ * Description: This function computes 
+ *the absolute value of the input integer @num.
+ * If @num is negative, it returns the 
+ *negation of @num to make it positive. If @num
  * is already non-negative, it returns @num as is.
  */
 int _abs(int num)
 {
-    if (num < 0)
-    {
-        return (-num);
-    }
-    else
-    {
-        return (num);
-    }
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
 }
