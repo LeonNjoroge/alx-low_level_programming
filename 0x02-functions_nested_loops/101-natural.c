27d9aeaa08528056218fbdef5c5fb1e21ceb21d6
Leon <leonmuraguri@gmail.com>
2023-09-12 12:33:15 -0700

    Solution 14 - 0

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+    int addition = 0; // Initialize addition to zero
+
+    for (i = 0; i < 1024; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+            addition = addition + i;
+    }
+
+    printf("%d\n", addition);
+    return (0);
+}
