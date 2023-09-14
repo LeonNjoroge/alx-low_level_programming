1b0eeb78cd6f846189d1b324695fd1ad7026932b
Leon <leonmuraguri@gmail.com>
2023-09-14 13:05:51 -0700

    Solutions 2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,14 @@
 #include "main.h"
-
-
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
 void print_line(int n)
 {
-
-
-  if (n <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      for (;n > 0;n--)
-	{
-	  _putchar('_');
-	}
-    }
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
