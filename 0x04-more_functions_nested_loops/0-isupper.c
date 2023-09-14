a83517149af22ba4e39510e75384a02728136b11
Leon <leonmuraguri@gmail.com>
2023-09-13 22:11:54 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+int _isupper(int c)
+{
+  if (c >= 'a' && c <= 'z')
+        {
+                return (0);
+        }
+        else
+        {
+                return (1);
+        }
+
+}
