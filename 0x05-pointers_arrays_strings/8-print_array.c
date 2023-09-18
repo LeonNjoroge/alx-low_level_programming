b9a1aa4874f7448996b0bcefa3885e244db046a2
Leon <leonmuraguri@gmail.com>
2023-09-17 23:31:25 -0700

    Solution 8 - 3

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
+0;276;0c
 void print_array(int *a, int n) {
   int i = 0;
   for (; i < n; i++) {
         if (i > 0) {
-            _putchar(', '); 
+            _putchar(','); 
         }
         _putchar( a[i]); 
     }
