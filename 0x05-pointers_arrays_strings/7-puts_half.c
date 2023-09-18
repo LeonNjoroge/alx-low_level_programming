a29571c3537870fd7c3f2f552f8a51080bd5f77c
Leon <leonmuraguri@gmail.com>
2023-09-17 23:21:56 -0700

    Solution 7 - 1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+void puts_half(char *str)
+{
+  int len = strlen(str);
+  int start = (len - 1) / 2
+    int i = start;
+
+  for (; i < len ; i++)
+    {
+      putchar(str[i]);
+    }
+  
+
+
+}
