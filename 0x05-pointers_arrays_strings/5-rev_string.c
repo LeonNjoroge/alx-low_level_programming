cad7f673b374e65242f8655884dbe15f009cfb4f
Leon <leonmuraguri@gmail.com>
2023-09-18 16:28:09 -0700

    Solution 6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,26 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * rev_string - Reverses a string in place.
+ *
+ * @s: A pointer to a string.
+ *
+ * This function takes a pointer to a string and reverses the characters
+ * in the string in place.
+ */
+void rev_string(char *s)
+{
+int length = strlen(s);
+int left = 0;
+int right = length - 1;
 
-void rev_string(char *s) {
-    int length = strlen(s);
-    int left = 0;
-    int right = length - 1;
-    
-    while (left < right) {
-       
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        
-        left++;
-        right--;
-    }
+while (left < right)
+{
+char temp = s[left];
+s[left] = s[right];
+s[right] = temp;
+
+left++;
+right--;
 }
