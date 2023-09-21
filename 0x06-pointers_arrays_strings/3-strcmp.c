4b9cc27ee533487a19115e1fa44709613ae6f350
Leon <leonmuraguri@gmail.com>
2023-09-21 05:48:00 -0700

    Solution 3 - 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,10 @@ int _strcmp(char *s1, char *s2)
     {
       if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 	break;
+      
       j = *(s1 + i) - *(s2 + i);
       i++;
     }
   return(j);
-
-    }
 
 }
