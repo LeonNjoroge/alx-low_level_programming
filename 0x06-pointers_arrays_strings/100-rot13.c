c48ef587c4d9cd8a4e5ff75f8c819e574c40dc99
Leon <leonmuraguri@gmail.com>
2023-09-21 08:23:41 -0700

    Added solutions

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *rot13(char *s)
+{
+	int a = 0, i;
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + a) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + a) == letters[i])
+			{
+				*(s + a) = rot13[i];
+				break;
+			}
+		}
+		a++;
+	}
+
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
