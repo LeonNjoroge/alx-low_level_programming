74a863309d9d682fda8994c5b464ccc81a432d40
Leon <leonmuraguri@gmail.com>
2023-09-21 08:29:46 -0700

    Betty checks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string to append to dest.
+ * 
+ * Return: A pointer to the resulting string.
+ */
 char *_strcat(char *dest, char *src)
 {
-  int s1 = 0;
-  int s2 = 0;
-  
-  while(*(dest + s1) != '\0')
-    {
-      s1++;
+	int s1 = 0;
+	int s2 = 0;
 
-    }
+	while (*(dest + s1) != '\0')
+	{
+		s1++;
+	}
 
-  while (s2 >= 0)
+	while (s2 >= 0)
 	{
 		*(dest + s1) = *(src + s2);
 		if (*(src + s2) == '\0')
@@ -20,6 +26,5 @@ char *_strcat(char *dest, char *src)
 		s2++;
 	}
 
-  
-  return (dest);
+	return (dest);
 }
