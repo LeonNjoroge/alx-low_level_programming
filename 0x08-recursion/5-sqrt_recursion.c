fa3190ff9cd5fd84db9ce86cc867947793495000
Leon <leonmuraguri@gmail.com>
2023-09-27 09:57:45 -0700

    Solution 5 - 2

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,31 @@
 #include "main.h"
 
+int pow_op(int n, int a)
+{
+if (a % (n / a) == 0)
+	{
+		if (a * (n / a) == n)
+			return (a);
+		else
+			return (-1);
+	}
+	return (0 + pow_op(n, a + 1));
+
+
+}
+
+
+
 int _sqrt_recursion(int n)
 {
+  if (n < 0)
+    return (-1);
+  if (n == 0)
+    return (0);
+  if (n == 1)
+    return (1);
 
+  return pow_op(n,2);
 
   
 }
