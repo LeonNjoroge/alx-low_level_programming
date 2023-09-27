f7e98dce02c9e80ab60efb94d52447ae91d6981b
Leon <leonmuraguri@gmail.com>
2023-09-27 09:42:37 -0700

    Solution 7 - 1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,28 @@
 #include "main.h"
 #include <string.h>
 
+int check_palindrome(char *s,int left, int right)
+{
+  if(left < right)
+    return (1);
+  
+  if (s[left] != s[right])
+        {
+	  return (0);
+        }
+  return (check_palindrome(s, left+1, right - 1));
+
+
+}
+
+
+
 int is_palindrome(char *s)
 {
-    int left = 0;
-    int right = strlen(s) - 1;
+  int length = strlen(s);
+  
+  if (length == 0)
+    return (1);
 
-    while (left < right)
-    {
-      if (s[left] != s[right])
-        {
-            return 0;
-        }
-
-        left++;
-        right--;
-    }
-
-    return 1; 
-
+  return check_palindrome(s, 0, length - 1);
 }
