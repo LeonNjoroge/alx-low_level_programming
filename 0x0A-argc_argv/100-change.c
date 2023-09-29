27b4ae90b3c1b4f7410e07a3be1a5a2ed58a97fc
Leon <leonmuraguri@gmail.com>
2023-09-29 15:37:26 -0700

    Solution 5 - 0

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[])
+{
+	int cent, coins = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cent = atoi(argv[1]);
+
+        for (;cent > 0; coins += 1;)
+	{
+		if (cent >= 25)
+			cent -= 25;
+		else if (cent >= 10)
+			cent -= 10;
+		else if (cent >= 5)
+			cent -= 5;
+		else if (cent >= 2)
+			cent -= 2;
+		else if (cent >= 1)
+			cent -= 1;
+		
+	}
+	printf("%d\n", coins);
+	return (0);
+}
