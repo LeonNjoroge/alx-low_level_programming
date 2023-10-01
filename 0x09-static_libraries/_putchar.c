408208321dcedbf403bce29611ab1fb1fbeba051
Leon <leonmuraguri@gmail.com>
2023-10-01 10:24:05 -0700

    Solution 0 - 1

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,23 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - Entry point of the program.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Description: This is the main function of the program. It prints the
- * string "_putchar" character by character using the `_putchar` function
- * and returns 0 to indicate successful execution.
- *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char str[] = "_putchar\n";
-	int i = 0;
-
-	for (; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-
-	return (0);
+	return (write(1, &c, 1));
 }
