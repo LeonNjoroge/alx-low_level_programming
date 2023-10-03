b3ff2a7dca345a6431d71719db6d9cd1fa3445bf
Leon <leonmuraguri@gmail.com>
2023-10-03 13:51:25 -0700

    Solution 2 - 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,57 +1,56 @@
 #include "main.h"
-#include <stdio.h>
+11;rgb:0000/0000/0000#include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * str_concat - Concatenates two strings.
+ *
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer to the concatenated string.
+ */
 char *str_concat(char *s1, char *s2)
 {
-  char *tmp1, *tmp2, *val;
-  unsigned int i, j, len1, len2;
-  
-  tmp1 = s1;
-  tmp2 = s2;
+char *tmp1, *tmp2, *val;
+unsigned int i, j, len1, len2;
 
-  if (s1 == NULL)
-    s1 = "";
+tmp1 = s1;
+tmp2 = s2;
 
-  if (s2 == NULL)
-    s2 = "";
+if (s1 == NULL)
+s1 = "";
 
-  i = 0;
-  j = 0;
+if (s2 == NULL)
+s2 = "";
 
-  while(*s1++)
-    {
-      i++;
-    }
-  while(*s2++)
-    {
-      j++;
-    }
+i = 0;
+j = 0;
 
-  s1 = tmp1;
-  s2 = tmp2;
-  len1 = i;
-  len2 = j;
+while (*s1++)
+i++;
 
-  val = malloc((len1 + len2)* sizeof(char)+ 1);
+while (*s2++)
+j++;
 
-  if (val == NULL)
-    return(NULL);
+s1 = tmp1;
+s2 = tmp2;
+len1 = i;
+len2 = j;
+val = malloc((len1 + len2) * sizeof(char) + 1);
 
-  i = 0;
-  j = 0;
+if (val == NULL)
+return (NULL);
 
-  for(; i < len1; i++)
-    {
-      val[i] = s1[i];
-    }
-  
-  for(; i < len1 + len2; i++)
-    {
-      val[i] = s2[i - len1];
-    }
+i = 0;
+j = 0;
 
-  val[i] = '\0';
-  return (val);
+for (; i < len1; i++)
+val[i] = s1[i];
+
+for (; i < len1 + len2; i++)
+val[i] = s2[i - len1];
+
+val[i] = '\0';
+return (val);
 }
