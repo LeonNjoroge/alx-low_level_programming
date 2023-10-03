ab22528747d1020c32b79520b98a91aa4a87112f
Leon <leonmuraguri@gmail.com>
2023-10-03 13:33:00 -0700

    Solution 6 - 1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char **strtow(char *str)
 {
@@ -12,59 +12,58 @@ char **strtow(char *str)
 
   a = 0;
   num = 0;
-  
+
   for (; str[a] != '\0'; a++)
-    {
-      if (str[a] != ' ' && (str[a + 1] == ' ' || str[a + 1] == '\0'))
-	num++;
-    }
+  {
+    if (str[a] != ' ' && (str[a + 1] == ' ' || str[a + 1] == '\0'))
+      num++;
+  }
 
   if (num == 0)
     return (NULL);
 
- 
-  
-  args = malloc((num + 1) * sizeof(char *));
+  args = (char **)malloc((num + 1) * sizeof(char *));
 
-  if(args == NULL)
-    {
-      free(args);
-      return (NULL);
-    }
+  if (args == NULL)
+  {
+    free(args);
+    return (NULL);
+  }
 
   a = 0;
-  
+  c = 0;
+
   for (; str[a] != '\0' && c < num; a++)
+  {
+    if (str[a] != ' ')
     {
-      if (str[a] != ' ')
-	{
-	  size = 0;
+      size = 0;
 
-	  for (b = a; str[b] != ' ' && str[b] != '\0'; b++)
-	    {
-	      size++;
-	      args[b] = malloc((size + 1) * sizeof(char));
+      for (b = a; str[b] != ' ' && str[b] != '\0'; b++)
+      {
+        size++;
+      }
 
-	      if (args[b] == NULL)
-		{
-		  e = 0;
+      args[c] = (char *)malloc((size + 1) * sizeof(char));
 
-		  for (; e < c; e++)
-		    free(args[c]);
+      if (args[c] == NULL)
+      {
+        for (e = 0; e < c; e++)
+          free(args[e]);
 
-		  free(args);
-		  return (NULL);
-		}
-	      d = 0;
-	      for (; d < size; d++, a++)
-		args[c][d] = str[a];
+        free(args);
+        return (NULL);
+      }
 
-	      args[c][d] = '\0', c++;
-			       
-	    }
-	}
-      
+      for (d = 0; d < size; d++, a++)
+      {
+        args[c][d] = str[a];
+      }
+
+      args[c][d] = '\0';
+      c++;
     }
+  }
   args[c] = NULL;
   return (args);
 }
