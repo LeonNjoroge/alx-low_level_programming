77c1c0f8d6c24388ceb5f8481b7666ffe2b745f4
Leon <leonmuraguri@gmail.com>
2023-10-03 01:26:00 -0700

    Solution 4 - 0

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_grid(int **grid, int height)
+{
+  int i;
+
+  i = 0;
+
+  while(i < height)
+    {
+      free(grid[i]);
+      i++;
+    }
+  free(grid);
+}
