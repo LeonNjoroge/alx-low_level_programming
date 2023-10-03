18f1cfaf43d287ab89b2d44c0d34f44f62b31d08
Leon <leonmuraguri@gmail.com>
2023-10-03 13:36:16 -0700

    Solution 6 - 2

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,55 +1,42 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 char *argstostr(int ac, char **av)
 {
-  int a, b, c, len;
+  int a, b, c = 0, len = 0;
   char *concat;
 
   if (ac == 0 || av == NULL)
     return (NULL);
 
-  a = 0;
-  b = 0;
-
-  while (a < ac)
+  for (a = 0; a < ac; a++)
+  {
+    for (b = 0; av[a][b] != '\0'; b++)
     {
-      while (av[a][b] != '\0')
-	{
-	  len++;
-	  b++;
-	}
       len++;
-      a++;
     }
+    len++;
+  }
   len++;
 
-  concat = malloc(len * sizeof(char));
+  concat = (char *)malloc(len * sizeof(char));
 
   if (concat == NULL)
-    {
-      free(concat);
-      return (NULL);
-      
-    }
+  {
+    return (NULL);
+  }
 
-  c = 0;
-  a = 0;
-  b = 0;
-
-  while(a < ac)
+  for (a = 0; a < ac; a++)
+  {
+    for (b = 0; av[a][b] != '\0'; b++)
     {
-      while (av[a][b] != '\0')
-	{
-	  concat[c] = av[a][b];
-	  b++;
-	  c++;
-	}
-      
-      concat[c] = '\n';
+      concat[c] = av[a][b];
       c++;
-      a++;
     }
+    concat[c] = '\n';
+    c++;
+  }
+  concat[c] = '\0';
+
   return (concat);
 }
