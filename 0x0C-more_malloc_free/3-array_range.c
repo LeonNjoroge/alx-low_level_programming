1e22388b00d1b0b96d11030a38bb9e2628609c3d
Leon <leonmuraguri@gmail.com>
2023-10-05 12:13:54 -0700

    Solution 3 - 0

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,30 +6,24 @@ int *array_range(int min, int max)
 {
 
   int *arr;
-  int len = 0, tmp;
-
-  tmp = min;
+  int len = 0, tmp = 0;
   
   if (min > max)
     return (NULL);
   
-  while(min <= max)
-    {
-    min++;
-    len++;
-    }
-  min = tmp;
+  len = max - min + 1;
   
+ 
   arr = malloc(len * sizeof(int));
   
   if (arr == NULL)
     return (NULL);
 
-  tmp = 0;
+ 
   while (tmp < len)
     arr[tmp] = min++;
 
-  arr[tmp] = '\0';
+
   
   return (arr);
 
