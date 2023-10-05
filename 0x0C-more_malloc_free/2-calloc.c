21b039b4ebfe4de12d0c2b3d8b3659b8b86b9509
Leon <leonmuraguri@gmail.com>
2023-10-05 11:42:43 -0700

    Solution 2 - 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
   if (nmemb == 0 || size == 0)
     return (NULL);
   
-  val = malloc(nmembb * size);
+  val = malloc(nmemb * size);
 
   if (val == NULL)
     return (NULL);
