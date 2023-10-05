9625daf3bad5fb84ce41d22117c1d5c2acfd86e3
Leon <leonmuraguri@gmail.com>
2023-10-05 09:52:55 -0700

    Solution 0 - 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void *malloc_checked(unsigned int b)
+{
+
+  int *val;
+  val = malloc( b * sizeof(unsigned int));
+
+  if (val == NULL)
+    exit(98);
+  return (val);
+  
+
+
+
+}
