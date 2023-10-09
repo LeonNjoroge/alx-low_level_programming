814aaecb5c82d5df904655073b5c9be45d55f99c
Leon <leonmuraguri@gmail.com>
2023-10-09 05:23:25 -0700

    Betty Check

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,21 @@
 #include <unistd.h>
 #include <string.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: This program prints
+ * the name of the source file to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  const char *name = __FILE__;
-  size_t size = strlen(name);
-  write(1, name, size);
-  write(1, "\n", 1);
-  return (0);
+const char *name = __FILE__;
+size_t size = strlen(name);
 
+write(1, name, size);
+write(1, "\n", 1);
+
+return (0);
 }
