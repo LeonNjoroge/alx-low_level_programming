aab73b135716831059a987b5cf0236c97f4e4273
Leon <leonmuraguri@gmail.com>
2023-10-09 09:15:36 -0700

    Betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,29 +1,34 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * print_dog - Prints the information about a dog
+ * @d: Pointer to the dog structure to print
+ *
+ * Description: This function prints the name, age, and owner of a dog.
+ * If the name or owner is NULL, it prints "(nil)" for that field.
+ */
+
 void print_dog(struct dog *d)
 {
 if (d)
 {
-  if(d->name != NULL)
-    {
-      printf("Name: %s\n", d->name);
-    }
-  else
-    {
-      printf("Name: (nil)\n");
-    }
-  printf("Age: %f\n", d->age);
-  if (d->owner)
-    {
-      printf("Owner: %s\n", d->owner);
-    }
-  else
-    {
-      printf("Owner: (nil)\n");
-    }
+if (d->name != NULL)
+{
+printf("Name: %s\n", d->name);
 }
-
-
-
+else
+{
+printf("Name: (nil)\n");
+}
+printf("Age: %f\n", d->age);
+if (d->owner)
+{
+printf("Owner: %s\n", d->owner);
+}
+else
+{
+printf("Owner: (nil)\n");
+}
+}
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,24 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - Frees memory allocated for a dog structure
+ * @d: Pointer to the dog structure to free
+ *
+ * Description: This function frees
+ * the memory allocated for a dog structure,
+ * including the memory allocated
+ * for the name, owner, and the structure itself.
+ */
+
 void free_dog(dog_t *d)
 {
 if (d)
 {
-  if (d->name)
-    free(d->name);
-  if (d->owner)
-    free(d->owner);
-  free(d);
+if (d->name)
+free(d->name);
+if (d->owner)
+free(d->owner);
+free(d);
 }
-
 }
