1c77bc5cbc8e5110b5de0adf5ed34ac194ac2e30
Leon <leonmuraguri@gmail.com>
2023-10-09 06:26:16 -0700

    Solution 0 - 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
+
+
+
 typedef struct dog
 {
-  char *name;
-  float age;
-  char *owner;
-} dog_t;
+char *name;
+float age;
+char *owner;
+}
+dog_t;
 
 
 
