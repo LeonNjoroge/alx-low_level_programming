833e7283855f6079247a5bd5c3174294958b3fef
Leon <leonmuraguri@gmail.com>
2023-10-09 09:22:58 -0700

    Solution 4 - 0

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,52 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * new_dog - Creates a new dog structure
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- *
- * Description: This function
- * creates a new dog structure and initializes its
- * name, age, and owner fields.
- *
- * Return: A pointer to the newly created dog structure.
- */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int a = 0, b = 0, c;
-dog_t *dogo;
+  int a = 0, b = 0 , c;
+  dog_t *dogo;
 
-while (name[a] != '\0')
-a++;
-while (owner[b] != '\0')
-b++;
+  while (name[a] != '\0')
+    a++;
+  while (owner[b] != '\0')
+    b++;
 
-dogo = malloc(sizeof(dog_t));
+  dogo = malloc(sizeof(dog_t));
 
-if (dogo == NULL)
-{
-free(dogo);
-return (NULL);
-}
+  if (dogo == NULL)
+    {
+      free(dogo);
+      return (NULL);
+    }
 
-dogo->name = malloc((a + 1) * sizeof(char));
-if (dogo->name == NULL)
-{
-free(dogo);
-return (NULL);
-}
+  dogo->name = malloc(a * sizeof(dogo->name));
+  if (dogo->name == NULL)
+    {
+      free(dogo->name);
+      free(dogo);
+      return (NULL);
+    }
+  c = 0;
+  while (c <= b)
+    {
+      dogo->owner[c] = owner[c];
+      c++;
+    }
+  return (dogo);
 
-c = 0;
-while (c <= b)
-{
-dogo->owner[c] = owner[c];
-c++;
-}
 
-dogo->age = age;
-
-return (dogo);
 }
