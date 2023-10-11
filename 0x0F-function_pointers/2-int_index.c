20cf6f7e0eeb3b71d94be97c2bbffcfb71f93ff5
Leon <leonmuraguri@gmail.com>
2023-10-11 10:23:12 -0700

    Solution 2 - 0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,12 +6,7 @@
  * @size: The size of the array
  * @cmp: A pointer to the comparison function
  *
- * Description: This function searches for an integer in the given array by
- * applying the comparison function to each element. If the array, comparison
- * function, and size are valid and the comparison function returns true for an
- * element, the index of that element is returned. If no match is found, or
- * if size is less than or equal to 0, -1 is returned.
- *
+ * Description: This function searches for an integer in the given array
  * Return: The index of the first matching element or -1 if no match or invalid
  * parameters.
  */
@@ -20,17 +15,21 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int a;
 a = 0;
-
+ 
 if (array && cmp)
 {
 if (size <= 0)
 {
 return (-1);
 }
+
 while (a < size)
 {
 if (cmp(array[a]))
+{
 return (a);
+}
+a++;
 }
 }
 return (-1);
