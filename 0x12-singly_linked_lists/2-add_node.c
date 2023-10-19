a63259cda91f6b81ee29abafa58f99dc0d3ecfd3
Leon <leonmuraguri@gmail.com>
2023-10-19 02:32:48 -0700

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+  if (str == NULL)
+    return NULL;
+  
+  list_t *newNode = malloc(sizeof(list_t));
+
+  if (newNode == NULL)
+    return NULL;
+  
+
+  newNode->str = strup(str);
+
+  if (newNode->str ==NULL)
+    {
+      free(newNode);
+      return NULL;
+    }
+  newNode->next = *head;
+  *head = newNode;
+  return newNode;
+
+}
