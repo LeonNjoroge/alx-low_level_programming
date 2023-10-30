20c4c6bed27a253c87b74d08354f0908c2af2652
Leon <leonmuraguri@gmail.com>
2023-10-30 04:39:34 -0700

    Tasks complete

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,74 @@
 #include "main.h"
 
 /**
- * file_error - Handles file-related errors and exits the program with specific codes.
+ * file_error - Handles file-related errors and exits
+ * the program with specific codes.
  * @f_f: File descriptor for the source file.
  * @f_t: File descriptor for the target file.
  * @argv: An array of command-line arguments.
  */
+
 void file_error(int f_f, int f_t, char *argv[])
 {
-    if (f_f == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    if (f_t == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
+if (f_f == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (f_t == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
 }
 
 /**
  * main - Entry point for a program to copy the content of one file to another.
  * @argc: The number of command-line arguments.
- * @argv: An array of command-line arguments including source and target file names.
+ * @argv: An array of command-line arguments
+ * including source and target file names.
  *
- * Return: On success, it returns 0. If an error occurs, it exits with an appropriate exit code.
+* Return: On success, it returns 0. If an error occurs
+* it exits with an appropriate exit code.
  */
+
 int main(int argc, char *argv[])
 {
-    int f_f, f_t, e_cl;
-    ssize_t cns, wn;
-    char c[1024];
+ssize_t cns, wn;
+int f_f, f_t, e_cl;
+char c[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "%s\n", "Usage: cp f_f f_t");
+exit(97);
+}
+f_f = open(argv[1], O_RDONLY);
+f_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+file_error(f_f, f_t, argv);
+cns = 1024;
+while (cns == 1024 && cns > 0)
+{
+cns = read(f_f, c, 1024);
+if (cns == -1)
+file_error(-1, 0, argv);
+wn = write(f_t, c, cns);
+if (wn == -1)
+file_error(0, -1, argv);
+}
+e_cl = close(f_f);
+if (e_cl == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_f);
+exit(100);
+}
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
-        exit(97);
-    }
+e_cl = close(f_t);
 
-    f_f = open(argv[1], O_RDONLY);
-    if (f_f == -1)
-    {
-        file_error(-1, 0, argv);
-    }
+if (e_cl == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_t);
+exit(100);
+}
+return (0);
 
-    f_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (f_t == -1)
-    {
-        file_error(0, -1, argv);
-    }
-
-    while ((cns = read(f_f, c, 1024)) > 0)
-    {
-        wn = write(f_t, c, cns);
-        if (wn == -1)
-        {
-            file_error(0, -1, argv);
-        }
-    }
-
-    if (cns == -1)
-    {
-        file_error(-1, 0, argv);
-    }
-
-    e_cl = close(f_f);
-    if (e_cl == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_f);
-        exit(100);
-    }
-
-    e_cl = close(f_t);
-    if (e_cl == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_t);
-        exit(100);
-    }
-
-    return (0);
 }
