a2da9ba90a5a60af632d4c32260ad14b26a21a56
LeonNjoroge <leonmuraguri@gmail.com>
2024-04-10 21:08:45 +0300

    Done

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,6 @@
   * Description: Prints a value every time it is compared in the array.
   * Uses the square root of the array size as the jump step.
   */
- 
 int jump_search(int *array, size_t size, int value)
 {
 	size_t m, jump, step;
